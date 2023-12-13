5df0fc30fc1fcadbad3e7faedc67c5e8ff32e45b
axios-shosan <medmohamed923@gmail.com>
2023-12-13 23:02:31 +0100

    finished Gallery, reading images from gallery

--- lib/app/modules/gallery/controllers/gallery_controller.dart
+++ lib/app/modules/gallery/controllers/gallery_controller.dart
@@ -1,17 +1,24 @@
+import 'dart:io';
+
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class GalleryController extends GetxController {
   //TODO: Implement GalleryController
 
   final count = 0.obs;
+  var imageList = [].obs;
+
   @override
-  void onInit() {
+  Future<void> onInit() async {
     super.onInit();
+    await pickImages();
   }
 
   @override
-  void onReady() {
+  Future<void> onReady() async {
     super.onReady();
+    await pickImages();
   }
 
   @override
@@ -19,5 +26,10 @@ class GalleryController extends GetxController {
     super.onClose();
   }
 
-  void increment() => count.value++;
+  Future<void> pickImages() async {
+    List<XFile>? pickedImages = await ImagePicker().pickMultiImage();
+    if (pickedImages != null) {
+      imageList.value.addAll(pickedImages.map((XFile image) => File(image.path)));
+    }
+  }
 }
--- lib/app/modules/gallery/views/gallery_view.dart
+++ lib/app/modules/gallery/views/gallery_view.dart
@@ -1,12 +1,10 @@
 import 'package:bast3d/app/shared/BottomNavigationbar/bottomNavigationBar_view.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import '../controllers/gallery_controller.dart';
 
 class GalleryView extends GetView<GalleryController> {
-  const GalleryView({Key? key}) : super(key: key);
+  const GalleryView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,23 +13,16 @@ class GalleryView extends GetView<GalleryController> {
         centerTitle: true,
       ),
       body: Center(
-        child: GridView.count(crossAxisCount: 4,
-          children: List.generate(100, (index) {
-            return Center(
-                child: Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 2.0,
-                    ),
-                  ),
-                  child: Image.network('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
-                      width: 110.0, height: 110.0),
-                )
-            );
-          }),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 4.0,
+            mainAxisSpacing: 4.0,
+          ),
+          itemCount: controller.imageList.value.length,
+          itemBuilder: (context, index) {
+            return Image.file(controller.imageList.value[index]);
+          },
         ),
       ),
       bottomNavigationBar: const CustomBottomNavigationView(index: 1),
